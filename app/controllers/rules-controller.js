bbedb213f2440280279716f8ec59be6d1ddfcd38
Nicolas Cisco <ncis20@gmail.com>
2017-09-07 01:03:37 -0300

    Fixed add, execute rules. Added postman collection

--- app/controllers/rules-controller.js
+++ app/controllers/rules-controller.js
@@ -1,36 +1,45 @@
 const Rules = require('../services/rules-service');
+const serialize = require('serialize-javascript');
+const deserialize = str => eval(`(${str})`);
 
+const buildResponse = data => ({
+  status: 'OK',
+  data,
+});
 
-var response  = {
-	status: 'OK',
-	data:{}
-}
+const buildError = err => ({
+  status: 'ERROR',
+  error: err.toString(),
+});
 
 exports.executeRules = (req, res) => {
-	var args = req.body
-	Rules.execute(args)
-		.then((result) => {
-			response.data = result;
-	  		res.send(response)
-	  	})
+  const args = req.body
+  Rules.execute(args)
+    .then(r => {
+      console.log(r);
+      res.send(buildResponse(r))
+    });
 }
 
 exports.getRules = (req, res) => {
-	Rules.getRules()
-		.then((result) => {
-			console.log(result)
-			response.data = result;
-			console.log(response)
-	  		res.json(response)
-	  	})
+  Rules.getRules()
+    .then(result => {
+      console.log(result)
+      const response = buildResponse(serialize(result));
+      console.log(response)
+      res.json(response)
+    });
 }
 
-
 exports.addRule = (req, res) => {
-  	var args = req.body
-  	Rules.addRule(args)
-		.then((result) => {
-			response.data = result;
-	  		res.send(response)
-	  	})
+  try {
+    const args = deserialize(req.body.rule);
+    Rules.addRule(args)
+      .then(r => {
+        res.json(buildResponse(serialize(r)));
+      });
+  } catch(e) {
+    res.status(400)
+      .json(buildError(e));
+  }
 }
--- app/services/rules-service.js
+++ app/services/rules-service.js
@@ -1,61 +1,63 @@
 //import the package
-var RuleEngine = require('node-rules');
+const RuleEngine = require('node-rules');
 
-/*as you can see above we removed the priority 
-and on properties for this example as they are optional.*/ 
+/*as you can see above we removed the priority
+and on properties for this example as they are optional.*/
 
-//sample fact to run the rules on	
-var fact = {
-    "userIP": "27.3.4.5",
-    "name":"user4",
-    "application":"MOB2",
-    "userLoggedIn":true,
-    "transactionTotal":600,
-    "cardType":"Credit Card",
+//sample fact to run the rules on
+const fact = {
+  userIP: "27.3.4.5",
+  name: "user4",
+  application: "MOB2",
+  userLoggedIn: true,
+  transactionTotal: 600,
+  cardType: "Credit Card",
 };
 
 //define the rules
-var rules = [{
-	"condition": function(R) {
-		R.when(this && (this.transactionTotal < 500));
-	},
-	"consequence": function(R) {
-		this.result = false;
-		R.stop();
-	}
+const rules = [{
+  condition: function (R) {
+    R.when(this && (this.transactionTotal < 500));
+  },
+  consequence: function (R) {
+    this.result = false;
+    R.stop();
+  }
 }];
 
-
-var apply = (args, resolve) => {
-	var R = new RuleEngine(rules);
-	//Now pass the fact on to the rule engine for results
-	R.execute(fact,function(result){ 
-		var message = "Payment Rejected"; 
-		if(result.result) 
-			message = "Payment Accepted"; 
-		resolve(message)	
-	});
+const apply = (args, resolve) => {
+  const R = new RuleEngine(rules);
+  //Now pass the fact on to the rule engine for results
+  R.execute(fact, result => {
+    let message = "Payment Rejected";
+    if(result.result)
+      message = "Payment Accepted";
+    resolve({
+      message,
+      fact: result,
+    });
+  });
 }
 
-var getRules = (resolve) => {
-	resolve(rules)
+const getRules = resolve => {
+  resolve(rules);
 };
 
 module.exports = {
-	execute: (args) => {
-		return new Promise((resolve, reject) => {
-			apply(args,resolve)
-		})
-	},
-	addRule: (rule) => {
-		return new Promise((resolve, reject) => {
-			getRules(resolve)
-		})
-	},
-	getRules: () => {
-		return new Promise((resolve, reject) => {
-			getRules(resolve)
-		})
-	}
-
-}
+  execute: (args) => {
+    return new Promise((resolve, reject) => {
+      apply(args, resolve)
+    })
+  },
+  addRule: (rule) => {
+    rules.push(rule);
+    return new Promise((resolve, reject) => {
+      getRules(resolve)
+    })
+  },
+  getRules: () => {
+    return new Promise((resolve, reject) => {
+      getRules(resolve)
+    })
+  },
+};
